4eda879d15091aada4b41c304675defe32f4d3c8
DmDm <9111322054@mail.ru>
2022-02-21 15:56:33 +0300

    update

---
+++ ex-7-1-Linq_Student-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_7_1_Linq_Student_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
