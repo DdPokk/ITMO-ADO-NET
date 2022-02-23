44049eb832e5bd7009037f9f7b37f9b01805362f
DmDm <9111322054@mail.ru>
2022-02-23 17:45:27 +0300

    update 9.1

--- ex-8-4-LINQsql_m/Form1.Designer.cs
+++ ex-8-4-LINQsql_m/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(13, 13);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 45);
+            this.button1.Size = new System.Drawing.Size(255, 45);
             this.button1.TabIndex = 0;
             this.button1.Text = "Десять лучших";
             this.button1.UseVisualStyleBackColor = true;
@@ -71,7 +71,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(282, 425);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Name = "LINQsql_m";
